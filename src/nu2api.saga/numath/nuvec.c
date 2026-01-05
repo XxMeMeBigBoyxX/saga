5930b9581573d104f482fcc07103036c3af5027c
Avery Townsend <self@codeshaunted.com>
2026-01-05 12:50:44 -0800

    add progress script and some test impls

---
+++ src/nu2api.saga/numath/nuvec.c
@@ -0,0 +1,7 @@
+#include "nu2api.saga/numath/nuvec.h"
+
+void NuVecAdd(NUVEC* v, NUVEC* v0, NUVEC* v1) {
+    v->x = v0->x + v1->x;
+    v->y = v0->y + v1->y;
+    v->z = v0->z + v1->z;
+}
---
+++ src/nu2api.saga/numath/nuvec.h
@@ -0,0 +1,12 @@
+#ifndef NU2API_SAGA_NUMATH_NUVEC_H
+#define NU2API_SAGA_NUMATH_NUVEC_H
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} nuvec_s;
+
+typedef nuvec_s NUVEC;
+
+#endif // NU2API_SAGA_NUMATH_NUVEC_H
