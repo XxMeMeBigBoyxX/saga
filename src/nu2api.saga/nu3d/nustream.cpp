c1cad72a467b2a5b2cd06bc73d6e6a6f04f09039
Seán de Búrca <sdeburca@fastmail.net>
2026-02-02 20:49:00 -0800

    Tiny improvements to NuRndrStreamInit

--- src/nu2api.saga/nu3d/nustream.cpp
+++ src/nu2api.saga/nu3d/nustream.cpp
@@ -5,7 +5,7 @@
 #include "decomp.h"
 #include "nu2api.saga/numemory/numemory.h"
 
-int rndrstream_nbuffers = 2;
+unsigned int rndrstream_nbuffers = 2;
 int nurndr_maxstreamsize;
 VARIPTR rndrstream_end;
 VARIPTR rndrstream_free;
@@ -15,7 +15,7 @@ static int rndrstream_buffid;
 
 void NuRndrStreamInit(int stream_buffer_size, VARIPTR *buffer) {
     if (buffer == NULL) {
-        for (unsigned int i = 0; i < rndrstream_nbuffers; i++) {
+        for (int i = 0; i < rndrstream_nbuffers; i++) {
             rndrstream[i].void_ptr = NuMemoryGet()->GetThreadMem()->_BlockAlloc(stream_buffer_size, 4, 1, "", 0);
             memset(rndrstream[i].void_ptr, 0, stream_buffer_size);
         }
@@ -24,7 +24,7 @@ void NuRndrStreamInit(int stream_buffer_size, VARIPTR *buffer) {
             buffer->addr = ALIGN(buffer->addr, 128);
             rndrstream[i].void_ptr = buffer->void_ptr;
             memset(buffer->void_ptr, 0, stream_buffer_size);
-            buffer->addr = buffer->addr + stream_buffer_size;
+            buffer->void_ptr = (void *)(buffer->addr + stream_buffer_size);
         }
     }
 
